b5966284c86c82550dd1ed90342bd8fbb1510b84
celio <cmartinsaires@gmail.com>
2018-09-03 14:35:41 -0300

    Adicionar arquivos de projeto.

---
+++ EstudosGit/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace EstudosGit.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ EstudosGit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EstudosGit.Global" Language="C#" %>
